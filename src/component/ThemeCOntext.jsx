cc01639d635f6101f25586174eaad270e358b092
Yahaya Muhammad Yusuf <120266549+feexie@users.noreply.github.com>
2024-09-13 23:24:48 +0100

    themesetup and intigrated into the app

--- src/component/ThemeCOntext.jsx
+++ src/component/ThemeCOntext.jsx
@@ -1,11 +1,26 @@
-import React, { creareContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext();
 
 export const useTheme = ()  => use useContext(ThemeContext);
 
 export const ThemeProvider = ({children}) => {
-    const [theme, seTheme] = useState(
+    const [theme, setTheme] = useState(
     () => localStorage.getItem("thme") || "light"
 );
+
+useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
