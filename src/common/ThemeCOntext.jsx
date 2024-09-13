836228be69edfc0bad23530766d281c5beb9d23e
Yahaya Muhammad Yusuf <120266549+feexie@users.noreply.github.com>
2024-09-13 23:43:05 +0100

    theme added

--- src/common/ThemeCOntext.jsx
+++ src/common/ThemeCOntext.jsx
@@ -2,20 +2,20 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext();
 
-export const useTheme = ()  => use useContext(ThemeContext);
+export const useTheme = () => useContext(ThemeContext);
 
-export const ThemeProvider = ({children}) => {
-    const [theme, setTheme] = useState(
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(
     () => localStorage.getItem("thme") || "light"
-);
+  );
 
-useEffect(() => {
-    document.body.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,49 +1,62 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
-import themeIcon from "../../assets/sun.svg";
-import twitterIcon from "../../assets/twitter-light.svg";
-import githubIcon from "../../assets/github-light.svg";
-import LinkedinIcon from "../../assets/linkedin-light.svg";
+import heroImg from "../../assets/Design uten navn.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
 import CV from "../../assets/cv.pdf";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
   return (
-    <section id="hero" className={styles.colorModeContainer}>
+    <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img
-          className={styles.Hero}
           src={heroImg}
-          alt="Profile picture of Yusuf Yahaya "
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
         />
         <img
-          className={styles.colorModeContainer}
+          className={styles.colorMode}
           src={themeIcon}
-          alt="Colour mode icon"
+          alt="Color mode icon"
+          onClick={toggleTheme}
         />
       </div>
       <div className={styles.info}>
         <h1>
           Yusuf
           <br />
-          Yahaya
+          Yahaya Muhammad
         </h1>
-        <h2>frontend Developer</h2>
+        <h2>Frontend Developer</h2>
         <span>
-          <a href="http://twitter.com/" target="_blank">
+          <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="http://github.com/" target="_blank">
-            <img src={githubIcon} alt="github icon" />
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="http://Linkedin.com/" target="_blank">
-            <img src={LinkedinIcon} alt="Linkedin icon" />
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-        <p>
-          with a passion for developing modern React web apps for commercial
-          business.
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
         </p>
-        <a href={CV} downlload>
+        <a href={CV} download>
           <button className="hover">Resume</button>
         </a>
       </div>
