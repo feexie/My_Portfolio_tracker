82def17d258e273e2df4b85565004f9b01b73b94
Yahaya Muhammad Yusuf <120266549+feexie@users.noreply.github.com>
2024-09-13 12:33:34 +0100

    info tag creted/Hero compoenet

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,15 +1,28 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
+import themeIcon from "../../assets/sun.svg";
 
 function Hero() {
   return (
-    <section id="hero">
+    <section id="hero" className={styles.colorModeContainer}>
       <div className={styles.colorModeContainer}>
         <img
           className={styles.Hero}
           src={heroImg}
           alt="Profile picture of Yusuf Yahaya "
         />
+        <img
+          className={styles.colorModeContainer}
+          src={themeIcon}
+          alt="Colour mode icon"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Yusuf
+          <br />
+          Yahaya
+        </h1>
       </div>
     </section>
   );
