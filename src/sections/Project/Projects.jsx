d6315e19cab520c07858791f0062ce20dfcda671
Yahaya Muhammad Yusuf <120266549+feexie@users.noreply.github.com>
2024-09-14 11:52:51 +0100

    Hamburger Card Created

--- src/sections/Project/Projects.jsx
+++ src/sections/Project/Projects.jsx
@@ -1,5 +1,8 @@
 import styles from "./ProjectsStyles.module.css";
 import viberr from "../../assets/viberr.png";
+import freshBurger from "../../assets/fresh-burger.png";
+import hipsster from "../../assets/hipsster.png";
+import fitLift from "../../assets/fitlift.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -10,7 +13,14 @@ function Projects() {
         <ProjectCard
           src={viberr}
           link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="viber"
+          h3="Viberr"
+          p="Streaming App"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Fresh Burger"
+          p="Hamburger Restaurant"
         />
       </div>
     </section>
